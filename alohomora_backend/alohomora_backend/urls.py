cafd10ed6c06c7db1575f300f9f3c46d38d3158c
Harshit Khetan <khetanmehul@gmail.com>
2020-05-19 14:42:15 +0530

    Basic registration django app working

--- alohomora_backend/alohomora_backend/urls.py
+++ alohomora_backend/alohomora_backend/urls.py
@@ -1,21 +1,38 @@
-"""alohomora_backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url, include
+from django.views import generic
+from rest_framework.schemas import get_schema_view
+
+from rest_framework import views, serializers, status
+from rest_framework.response import Response
+from allauth.account.views import ConfirmEmailView
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view()
+def complete_view(request):
+    return Response("Email account is activated")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('alohomora/', include('alohomora.urls')),
+    url(r'^$', generic.RedirectView.as_view(url='/api/', permanent=False)),    
+    url(r'^api/$', get_schema_view()),
+    url(r'^api/registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
+    url(r'^api/registration/complete/$', complete_view, name='account_confirm_complete'),
+    url(r'^api/registration/', include('rest_auth.registration.urls')),
+
+    url(r'^api/auth/', include('rest_auth.urls')),
+    re_path(r'^api/auth/password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^api/auth/password/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),    
+ 
 ]
+
+
+
+
