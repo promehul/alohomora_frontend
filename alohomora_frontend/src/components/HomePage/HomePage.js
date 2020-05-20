4b53399de8fe09fceb75c1012a4ad91e5d9a52ab
Harshit Khetan <khetanmehul@gmail.com>
2020-05-20 23:29:57 +0530

    Add leaderboard in frontend

--- alohomora_frontend/src/components/HomePage/HomePage.js
+++ alohomora_frontend/src/components/HomePage/HomePage.js
@@ -2,25 +2,37 @@ import axios from "axios";
 import React, { Component } from "react";
 import "./HomePage.css";
 
-import {Sidebar, Menu, Segment, Icon, Button, Header, Image, Loader} from "semantic-ui-react"
+import {
+  Sidebar,
+  Menu,
+  Segment,
+  Icon,
+  Label,
+  Button,
+  Header,
+  Image,
+  Loader,
+} from "semantic-ui-react";
 
 import { AlohomoraUrls } from "../../constants/urls";
 import store from "../../store";
 import { getUserToken } from "../../utils/authUtils";
 
-import Question from "./Question"
+import Question from "./Question";
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       userInfo: {},
+      profileInfo: [],
       loaded: false,
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const selfUserUrl = AlohomoraUrls.SELF_USER;
+    const profileUrl = AlohomoraUrls.PROFILE;
     const token = getUserToken(store.getState());
     const headers = { authorization: "Token " + token };
 
@@ -29,14 +41,21 @@ class HomePage extends Component {
       url: selfUserUrl,
       headers: headers,
     });
-    
+
+    const reqToProfile = axios({
+      method: "get",
+      url: profileUrl,
+      headers: headers,
+    });
+
     if (token) {
       axios
-        .all([reqToSelfUser])
+        .all([reqToSelfUser, reqToProfile])
         .then(
-          axios.spread((profileRes) => {
+          axios.spread((selfUserRes, profileRes) => {
             this.setState({
-              userInfo: profileRes.data,
+              userInfo: selfUserRes.data,
+              profileInfo: profileRes.data.results,
               loaded: true,
             });
           })
@@ -46,14 +65,37 @@ class HomePage extends Component {
         });
     }
   }
-  
 
   render() {
     if (this.state.loaded) {
       return (
         <React.Fragment>
+          <Sidebar
+            as={Menu}
+            animation="overlay"
+            icon="labeled"
+            inverted
+            vertical
+            visible
+            width="wide"
+            direction="right"
+            style={{ width: 270 }}
+          >
+            <Label size="medium" color="white">
+              LeaderBoard
+            </Label>
+            {this.state.profileInfo.map((userprofile, index) => (
+              <Menu.Item header>
+                {index + 1}
+                {". "}
+                {userprofile.user.username}
+                <Label>{userprofile.question_on}</Label>
+              </Menu.Item>
+            ))}
+          </Sidebar>
+
           <div className="question-center">
-          <Question question_on={this.state.userInfo.question_on}/>
+            <Question question_on={this.state.userInfo.question_on} />
           </div>
         </React.Fragment>
       );
