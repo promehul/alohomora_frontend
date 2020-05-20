299e80807e3bbd7a7bcd57f586a6df8e8273586e
Harshit Khetan <khetanmehul@gmail.com>
2020-05-20 11:17:03 +0530

    Backend complete

--- alohomora_backend/alohomora/urls.py
+++ alohomora_backend/alohomora/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url, include
 
 from alohomora.views.user_profile_view import UserProfileViewSet
 from alohomora.views.question_view import QuestionViewSet
+from alohomora.views.check_answer import CheckAnswer
 
 router = routers.SimpleRouter()
 router.register(
@@ -20,4 +21,5 @@ router.register(
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('check_answer', CheckAnswer.as_view(), name = 'check_answer'),
 ]
