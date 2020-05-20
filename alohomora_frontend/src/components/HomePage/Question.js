f123ec4420f03895b362906ad673db246e68313b
Harshit Khetan <khetanmehul@gmail.com>
2020-05-20 20:45:17 +0530

    Check answer added

--- alohomora_frontend/src/components/HomePage/Question.js
+++ alohomora_frontend/src/components/HomePage/Question.js
@@ -1,19 +1,28 @@
 import axios from "axios";
 import React, { Component } from "react";
 
-import { Image, Container, Label, Input, Button, Divider, Segment } from "semantic-ui-react";
+import {
+  Image,
+  Container,
+  Label,
+  Input,
+  Button,
+  Divider,
+  Segment,
+} from "semantic-ui-react";
 
 import { AlohomoraUrls } from "../../constants/urls";
 import store from "../../store";
 import { getUserToken } from "../../utils/authUtils";
 
-import "./Question.css"
+import "./Question.css";
 
 class Question extends Component {
   constructor(props) {
     super(props);
     this.state = {
       questionInfo: {},
+      answer: "",
     };
   }
 
@@ -46,9 +55,48 @@ class Question extends Component {
     }
   }
 
+  answerChangeHandle = (e) => {
+    this.setState({
+      answer: e.target.value,
+    });
+  };
+
+  checkAnswer = () => {
+    const checkAnswerUrl = AlohomoraUrls.CHECK_ANSWER;
+    const token = getUserToken(store.getState());
+    const headers = { authorization: "Token " + token };
+
+    var formData = new FormData();
+    formData.append("answer", this.state.answer);
+
+    if (token) {
+      axios({
+        method: "post",
+        url: checkAnswerUrl,
+        data: formData,
+        headers: headers,
+      })
+        .then((response) => {
+          console.log(response.data);
+          this.setState({
+            answer: "",
+          });
+        })
+        .catch((error) => {
+          console.log("error");
+        });
+    }
+  };
+
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.checkAnswer();
+    }
+  };
+
   render() {
     return (
-      <Container textAlign="center" text >
+      <Container textAlign="center" text>
         {this.state.questionInfo.image !== null && (
           <Image
             centered
@@ -57,11 +105,22 @@ class Question extends Component {
           ></Image>
         )}
         {this.state.questionInfo.question_text !== "" && (
-          <Segment size="large" textAlign='center'>{this.state.questionInfo.question_text}</Segment>
-          
+          <Segment size="large" textAlign="center">
+            {this.state.questionInfo.question_text}
+          </Segment>
         )}
-        <Input focus placeholder="Answer here.." size="small" className="answer-input"/> 
-        <Button positive >Check Answer</Button> 
+        <Input
+          focus
+          placeholder="Answer here.."
+          size="small"
+          className="answer-input"
+          onChange={this.answerChangeHandle}
+          onKeyPress={this.handleKeyPress}
+          value={this.state.answer}
+        />
+        <Button positive onClick={this.checkAnswer}>
+          Check Answer
+        </Button>
       </Container>
     );
   }
