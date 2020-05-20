6481d2bd7a2442b8531b6e776bba6182321983be
Harshit Khetan <khetanmehul@gmail.com>
2020-05-20 18:35:39 +0530

    Add urls to backend in constants

--- alohomora_frontend/src/constants/urls.js
+++ alohomora_frontend/src/constants/urls.js
@@ -9,3 +9,11 @@ export const AuthUrls = {
   USER_ACTIVATION: `${ROOT_URL}/api/registration/account-confirm-email/`,
   USER_PROFILE: `${ROOT_URL}/rest-auth/user/`,
 };
+
+
+export const AlohomoraUrls = {
+  PROFILE: `${ROOT_URL}/alohomora/profile/`,
+  QUESTION: `${ROOT_URL}/alohomora/question/`,
+  CHECK_ANSWER: `${ROOT_URL}/alohomora/check_answer`,
+  SELF_USER: `${ROOT_URL}/alohomora/self_user`,
+}
