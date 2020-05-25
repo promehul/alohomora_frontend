486e6d4c5e760805ae7652f34aa40fe58c631f13
Harshit Khetan <khetanmehul@gmail.com>
2020-05-25 19:56:01 +0530

    Updates

---
+++ precache-manifest.044585dec29d7085c488dc01049a803f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d2432fd4ef790f07b3601dd46bb652",
+    "url": "/alohomora_frontend/index.html"
+  },
+  {
+    "revision": "cd73012f3c45cb4a62c7",
+    "url": "/alohomora_frontend/static/css/2.445919a4.chunk.css"
+  },
+  {
+    "revision": "d386528e915c3692ced1",
+    "url": "/alohomora_frontend/static/css/main.94837fda.chunk.css"
+  },
+  {
+    "revision": "cd73012f3c45cb4a62c7",
+    "url": "/alohomora_frontend/static/js/2.c4c5dbe5.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/alohomora_frontend/static/js/2.c4c5dbe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d386528e915c3692ced1",
+    "url": "/alohomora_frontend/static/js/main.bceb33c1.chunk.js"
+  },
+  {
+    "revision": "d132662775432cc127fc",
+    "url": "/alohomora_frontend/static/js/runtime-main.62d9cf17.js"
+  }
+]);
