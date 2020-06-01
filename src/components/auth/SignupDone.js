6c92f28c75660f3b8b861c403089d844ce96094e
Harshit Khetan <khetanmehul@gmail.com>
2020-06-02 03:14:59 +0530

    Remove bootstrap and add Header

--- src/components/auth/SignupDone.js
+++ src/components/auth/SignupDone.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import { Header } from "semantic-ui-react";
 
 export default class SignupDone extends Component {
-    render() {
-        return (
-            <h3 className="mx-5">
-                Thanks for your registration, please follow the link sent to your provided email to activate
-                your account.
-            </h3>
-        )
-    }
+  render() {
+    return (
+      <Header>
+        Thanks for your registration, please follow the link sent to your
+        provided email to activate your account.
+      </Header>
+    );
+  }
 }
