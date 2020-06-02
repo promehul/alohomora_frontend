9b955c663470a6c81c220fedd2b520a9373dd346
Harshit Khetan <khetanmehul@gmail.com>
2020-06-02 23:55:26 +0530

    Add on logout redirect to login

--- src/components/auth/Logout.js
+++ src/components/auth/Logout.js
@@ -2,22 +2,20 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
-
+import history from "../../utils/historyUtils";
 class Logout extends Component {
+  static propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+  };
 
-    static propTypes = {
-        logoutUser: PropTypes.func.isRequired
-    };
+  componentWillMount() {
+    this.props.logoutUser();
+    history.push("/");
+  }
 
-    componentWillMount() {
-        this.props.logoutUser();
-    }
-
-    render() {
-        return (
-            <h2>Sorry to see you go...</h2>
-        );
-    }
+  render() {
+    return <h2>Sorry to see you go...</h2>;
+  }
 }
 
 export default connect(null, { logoutUser })(Logout);
