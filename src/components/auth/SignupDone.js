b30525f1f97fb0eef6ac71565cc65d6e6c9334f7
Harshit Khetan <32803738+promehul@users.noreply.github.com>
2021-12-22 00:25:26 +0530

    Update SignupDone.js

--- src/components/auth/SignupDone.js
+++ src/components/auth/SignupDone.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Header } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 export default class SignupDone extends Component {
   render() {
     return (
       <Header>
-        Thanks for your registration, please follow the link sent to your
-        provided email to activate your account.
+        Thanks for your registration, Account activation has been made optional. 
+        You can directly go to <Link to="/login">Login Page</Link> to proceed
+        with the login.
       </Header>
     );
   }
